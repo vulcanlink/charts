770ae3f3ef0cd09ab60cac2cd3f949acfa3e1eb8
Leo Vigna <leo.vigna@gmail.com>
2020-06-08 23:06:49 +0200

    Update pre-commit

--- package.sh
+++ package.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # helm package
-helm package bitcoind
-helm package ipfs
-helm package lnd
-helm package parity
-helm package geth
-helm package blockchain
+#helm package bitcoind
+#helm package ipfs
+#helm package lnd
+#helm package parity
+#helm package geth
+#helm package blockchain
 
 helm package chainlink
 helm package chainlink-gas-updater
 helm package eth-failover
-helm package vulcan
 
 helm package chainlink-graphql
 helm package vulcan-api
-helm package vulcan-api-wss
 helm package vulcan-gateway
+
+helm package vulcan-chart
 
 # helm repo index
 helm repo index . 
